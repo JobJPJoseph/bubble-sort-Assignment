a7be0125cec990665a0e444066ee3e5b9a192c8d
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-11-08 00:54:00 -0500

    completed assignment

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,33 @@
 
 function bubbleSort(arr) {
 
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length - 1; j++) {
+
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        console.log(arr.join(","));
+      }
+
+    }
+
     // Iterate through the array
 
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
+        // console.log(arr.join(","));
 
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
+  }
 
+  return;
 }
 
 module.exports = bubbleSort;
